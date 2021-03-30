a390b0f31a2c56dac9403cc0564af3413695970b
Bongani <Bongtheman@outlook.com>
2021-03-30 13:16:01 +0100

    initial commit

---
+++ Basic_Tower.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_Defense_Game
+{
+    class Class1
+    {
+    }
+}
---
+++ GameRuntime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tower_Defense_Game
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
